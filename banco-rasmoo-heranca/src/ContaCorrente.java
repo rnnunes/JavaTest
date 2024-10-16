1ae5640ecfe49a97eb861848733b89dcab650061
rnnunes <ryannunes70@gmail.com>
2024-10-16 00:53:11 -0400

    extends

---
+++ banco-rasmoo-heranca/src/ContaCorrente.java
@@ -0,0 +1,8 @@
+public class ContaCorrente extends Conta {
+
+
+    public ContaCorrente(int agencia, int id_conta, Cliente titular) {
+        super(agencia, id_conta, titular);
+    }
+
+}
---
+++ banco-rasmoo-heranca/src/ContaPoupanca.java
@@ -0,0 +1,7 @@
+public class ContaPoupanca extends Conta{
+
+    public ContaPoupanca () {
+
+    }
+
+}
