a153ead8d1f80661e6d0009acb1943f622edfe1f
rnnunes <ryannunes70@gmail.com>
2024-10-31 00:18:00 -0400

    cmt

--- exercicios/src/Main.java
+++ exercicios/src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class ParesImpares {
+public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
